b811d4b457f92556fc7f953415282ff6a2287ecc
Hung <mongtamquoc2015@gmail.com>
2025-11-07 14:29:51 +0700

    refactor: remove cards in avo because it is feature for pro

--- app/avo/dashboards/overview.rb
+++ app/avo/dashboards/overview.rb
@@ -2,8 +2,4 @@ class Avo::Dashboards::Overview < Avo::Dashboards::BaseDashboard
   self.id = "overview"
   self.name = "Overview"
   self.grid_cols = 4
-
-  # def cards
-  #   card Avo::Cards::UsersCount
-  # end
 end
