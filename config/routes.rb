e2b7a7488a435801abf487457faf3cf4dd6b7e86
Hung <mongtamquoc2015@gmail.com>
2026-01-30 14:45:59 +0700

    add: seperate routes to api and admin files

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   get "up" => "rails/health#show", as: :rails_health_check
 
   root to: "home#index"
 
-  namespace :api do
-    namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth", controllers: {
-        sessions: "api/v1/auth/sessions"
-      }
-
-      resources :users,  only: [] do
-        get "me", on: :collection, to: "users#me"
-      end
-    end
-  end
+  draw(:admin)
+  draw(:api)
 end
